daa1ca36dcd41dd8f256e1602f5b168fce3e7705
erka <erdenebold10@gmail.com>
2022-02-28 10:01:31 +0800

    code update

---
+++ gww_code_update.sh
@@ -0,0 +1,10 @@
+cd /opt/odoo/odoov13
+sudo git pull
+sudo /etc/init.d/odoo-server stop
+sudo /etc/init.d/odoo-server stop
+sudo /etc/init.d/odoo-server stop
+sudo /etc/init.d/odoo-server stop
+sudo /etc/init.d/odoo-server stop
+sudo /etc/init.d/odoo-server stop
+sudo /etc/init.d/odoo-server stop
+sudo /etc/init.d/odoo-server start
